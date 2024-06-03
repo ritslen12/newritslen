e3bd25aab33b1d6591477994b97be9047b05ef6d
ritslen12 <richlynramos012@gmail.com>
2024-06-03 23:31:23 +0800

    master

--- app/Http/Controllers/RishaneController.php
+++ app/Http/Controllers/RishaneController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Employee;
+use App\Models\Rishane;
 use Laravel\Ui\Presets\React;
 use PDO;
 
--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container">
 
-        <h3 align="center" class="">Employee Editing List</h3>
+        <h3 align="center" class="">Employee Management</h3>
 
         <div class="row">
             <div class="col-md-2">
